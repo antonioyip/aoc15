f6bf139c6bf39fdd5b24c0479ade373f553a3d3e
Tony Y <antonioyip@gmail.com>
2018-06-28 10:49:18 -0700

    Find maximum length route

--- day9/day9.go
+++ day9/day9.go
@@ -6,18 +6,18 @@ import (
 	"strings"
 )
 
-type edge struct {
-	a string
-	b string
+type graph map[string]map[string]int
+
+func (g graph) add(key1 string, key2 string, value int) {
+	if _, ok := g[key1]; !ok {
+		g[key1] = make(map[string]int)
+	}
+	if _, ok := g[key2]; !ok {
+		g[key2] = make(map[string]int)
+	}
+	g[key1][key2] = value
+	g[key2][key1] = value
 }
-
-func (e edge) toString() string {
-	return e.a + " -> " + e.b
-}
-
-var graph = map[edge]int{}
-
-var cities = map[string]bool{}
 
 func main() {
 	inputs, err := ioutil.ReadFile("day9.input")
@@ -25,39 +25,60 @@ func main() {
 		panic(err)
 	}
 
+	citiesVisited := make(map[string]bool)
+	distanceGraph := graph{}
+
 	// parse the file
-	// create the graph object
-	// create a list of unique cities
+	// create the graph
 	lines := strings.Split(string(inputs), "\n")
 	for _, line := range lines {
 		var source string
 		var destination string
 		var distance int
 		fmt.Sscanf(line, "%s to %s = %d", &source, &destination, &distance)
-		graph[edge{source, destination}] = distance
-		graph[edge{destination, source}] = distance
 
-		if _, ok := cities[source]; !ok {
-			cities[source] = true
-		}
+		citiesVisited[source] = false
+		citiesVisited[destination] = false
 
-		if _, ok := cities[destination]; !ok {
-			cities[destination] = true
-		}
+		distanceGraph.add(source, destination, distance)
+	}
+
+	// print out city list
+	for city, visited := range citiesVisited {
+		fmt.Printf("%s %t\n", city, visited)
 	}
 
 	// print out the graph entries
-	// for e, v := range graph {
-	// 	fmt.Printf("%d: %s\n", v, e.toString())
-	// }
-
-	for c1 := range cities {
-		for c2 := range cities {
-			if c1 == c2 {
-				continue
-			}
-			fmt.Printf("%s -> %s\n", c1, c2)
+	for city1, row := range distanceGraph {
+		for city2, distance := range row {
+			fmt.Printf("%d: %s -> %s\n", distance, city1, city2)
 		}
 	}
 
+	totalDistance := 0
+	for city := range citiesVisited {
+		citiesVisited[city] = true
+		thisDistance := tsp(distanceGraph, city, citiesVisited)
+		if totalDistance < thisDistance {
+			totalDistance = thisDistance
+		}
+		citiesVisited[city] = false
+	}
+
+	fmt.Printf("Total distance %d\n", totalDistance)
+}
+
+func tsp(distanceGraph graph, currentCity string, visitedCities map[string]bool) int {
+	totalDistance := 0
+	for city, visited := range visitedCities {
+		if !visited {
+			visitedCities[city] = true
+			currentDistance := tsp(distanceGraph, city, visitedCities) + distanceGraph[currentCity][city]
+			if totalDistance < currentDistance {
+				totalDistance = currentDistance
+			}
+			visitedCities[city] = false
+		}
+	}
+	return totalDistance
 }
